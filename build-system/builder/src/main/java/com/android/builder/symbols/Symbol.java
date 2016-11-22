Iandmee/base_tools

Moved SymbolLoader to SymbolIO.
Made a few changes to the way symbols are currently
handled:

- Added symbol class to symbol.
- Instead of directly reading from the SymbolLoader,
the SymbolWriter will read from an intermediate format,
the SymbolTable.
- Instead of using SymbolLoader to read symbols, the
code to load has been moved to SymbolIo.

This should produce no semantic changes, but should allow
for an in-memory representation of the symbol information
(the symbol table) that is independent of the way it is
loaded.

Change-Id: I3ab52b70e8f7092480f558228d50a68da436984e
Test: Included.

build-system/builder/src/main/java/com/android/builder/symbols/Symbol.java
@@ -18,6 +18,7 @@ package com.android.builder.symbols;
 
 import com.android.annotations.NonNull;
 import com.google.common.base.Preconditions;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 /**
@@ -155,5 +156,27 @@ public class Symbol {
     @NonNull
     public String getJavaType() {
         return javaType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(resourceType, name, javaType, value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Symbol)) {
+            return false;
+        }
+
+        Symbol other = (Symbol) obj;
+        return Objects.equals(resourceType, other.resourceType)
+                && Objects.equals(name, other.name)
+                && Objects.equals(javaType, other.javaType)
+                && Objects.equals(value, other.value);
     }
 }

build-system/builder/src/main/java/com/android/builder/symbols/SymbolTable.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.builder.symbols;
+
+import com.android.annotations.NonNull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class SymbolTable {
+
+    /**
+     * All symbols mapped by IDs (see {@link #key(Symbol)}.
+     */
+    @NonNull
+    private Map<String, Symbol> symbols;
+
+    /**
+     * Creates a new, empty, symbol table.
+     */
+    public SymbolTable() {
+        symbols = new HashMap<>();
+    }
+
+    /**
+     * Obtains a unique key for a symbol.
+     *
+     * @param symbol the symbol
+     * @return the unique ID
+     */
+    @NonNull
+    private static String key(@NonNull Symbol symbol) {
+        return symbol.getResourceType() + " " + symbol.getName();
+    }
+
+    /**
+     * Obtains all symbols in the table.
+     *
+     * @return all symbols
+     */
+    @NonNull
+    public Iterable<Symbol> allSymbols() {
+        return new ArrayList<>(symbols.values());
+    }
+
+    /**
+     * Adds a symbol to the table.
+     *
+     * @param symbol the symbol to add
+     */
+    public void add(@NonNull Symbol symbol) {
+        symbols.put(key(symbol), symbol);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof SymbolTable)) {
+            return false;
+        }
+
+        SymbolTable other = (SymbolTable) obj;
+        return Objects.equals(symbols, other.symbols);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbols);
+    }
+}

kapabl/android-tools-base

Change-Id: I2f1c1caa3d3ebb95050ce397139859ed145ad198

build-system/builder/src/main/java/com/android/builder/symbols/Symbol.java
@@ -17,7 +17,9 @@
 package com.android.builder.symbols;
 
 import com.android.annotations.NonNull;
+import com.android.builder.dependency.HashCodeUtils;
 import com.google.common.base.Preconditions;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 /**
@@ -155,5 +157,27 @@ public class Symbol {
     @NonNull
     public String getJavaType() {
         return javaType;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeUtils.hashCode(resourceType, name, javaType, value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Symbol)) {
+            return false;
+        }
+
+        Symbol other = (Symbol) obj;
+        return Objects.equals(resourceType, other.resourceType)
+                && Objects.equals(name, other.name)
+                && Objects.equals(javaType, other.javaType)
+                && Objects.equals(value, other.value);
     }
 }

build-system/builder/src/main/java/com/android/builder/symbols/SymbolTable.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.builder.symbols;
+
+import com.android.annotations.NonNull;
+import com.android.builder.dependency.HashCodeUtils;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class SymbolTable {
+
+    /**
+     * All symbols mapped by IDs (see {@link #key(Symbol)}.
+     */
+    @NonNull
+    private Map<String, Symbol> symbols;
+
+    /**
+     * Creates a new, empty, symbol table.
+     */
+    public SymbolTable() {
+        symbols = new HashMap<>();
+    }
+
+    /**
+     * Obtains a unique key for a symbol.
+     *
+     * @param symbol the symbol
+     * @return the unique ID
+     */
+    @NonNull
+    private static String key(@NonNull Symbol symbol) {
+        return symbol.getResourceType() + " " + symbol.getName();
+    }
+
+    /**
+     * Obtains all symbols in the table.
+     *
+     * @return all symbols
+     */
+    @NonNull
+    public Set<Symbol> allSymbols() {
+        return new HashSet<>(symbols.values());
+    }
+
+    /**
+     * Adds a symbol to the table.
+     *
+     * @param symbol the symbol to add
+     */
+    public void add(@NonNull Symbol symbol) {
+        symbols.put(key(symbol), symbol);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof SymbolTable)) {
+            return false;
+        }
+
+        SymbolTable other = (SymbolTable) obj;
+        return Objects.equals(symbols, other.symbols);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeUtils.hashCode(symbols);
+    }
+}