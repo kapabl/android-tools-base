kapabl/android-tools-base

commit f8f97fef6edf28f360617eb19768d0036dde7ca3

Fix nullability of LocaleQualifier.getLangage()
The default configuration has a locale qualifier with a null language,
so we cannot assume it's not null.

Bug: N/A
Test: existing tests
Change-Id: I899f326a8b04974ce93f24993aa0a03bd61c0c9b

sdk-common/src/main/java/com/android/ide/common/resources/configuration/LocaleQualifier.java
@@ -21,7 +21,6 @@ import com.android.annotations.Nullable;
 import com.android.annotations.VisibleForTesting;
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
-
 import java.util.Iterator;
 import java.util.Locale;
 
@@ -50,7 +49,7 @@ public final class LocaleQualifier extends ResourceQualifier {
     private static final String CAR_DOCK_MODE = "car";
 
     @NonNull private String mFull;
-    @NonNull private String mLanguage;
+    @Nullable private String mLanguage;
     @Nullable private String mRegion;
     @Nullable private String mScript;
 
@@ -102,7 +101,7 @@ public final class LocaleQualifier extends ResourceQualifier {
      * @return is it valid?
      */
     private static boolean isValidAlpha2Code(@NonNull String str) {
-        return str.length() == 2 && CharMatcher.JAVA_LETTER.matchesAllOf(str);
+        return str.length() == 2 && CharMatcher.javaLetter().matchesAllOf(str);
     }
 
     /**
@@ -111,7 +110,7 @@ public final class LocaleQualifier extends ResourceQualifier {
      * @return is it valid?
      */
     private static boolean isValidAlpha3Code(@NonNull String str) {
-        return str.length() == 3 && CharMatcher.JAVA_LETTER.matchesAllOf(str);
+        return str.length() == 3 && CharMatcher.javaLetter().matchesAllOf(str);
     }
 
     /**
@@ -406,7 +405,7 @@ public final class LocaleQualifier extends ResourceQualifier {
     }
 
     public boolean hasLanguage() {
-        return !FAKE_VALUE.equals(mLanguage);
+        return mLanguage != null && !FAKE_VALUE.equals(mLanguage);
     }
 
     public boolean hasRegion() {
@@ -560,7 +559,7 @@ public final class LocaleQualifier extends ResourceQualifier {
         return null;
     }
 
-    @NonNull
+    @Nullable
     public String getLanguage() {
         return mLanguage;
     }
@@ -584,7 +583,10 @@ public final class LocaleQualifier extends ResourceQualifier {
     public boolean isMatchFor(ResourceQualifier qualifier) {
         if (qualifier instanceof LocaleQualifier) {
             LocaleQualifier other = (LocaleQualifier)qualifier;
-            if (!mLanguage.equals(other.mLanguage)) {
+
+            if (mLanguage != null
+                    && other.mLanguage != null
+                    && !mLanguage.equals(other.mLanguage)) {
                 return false;
             }
 

lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/LocaleFolderDetector.java
@@ -200,6 +200,7 @@ public class LocaleFolderDetector extends Detector implements ResourceFolderScan
                 context.getFolderConfiguration().getLocaleQualifier() : null;
         if (locale != null && locale.hasLanguage()) {
             final String language = locale.getLanguage();
+            assert language != null; // hasLanguage() above.
             String replace = null;
             switch (language) {
                 case "he":
@@ -386,6 +387,7 @@ public class LocaleFolderDetector extends Detector implements ResourceFolderScan
                 assert locale != null : folderName;
                 configToFile.put(config, mBcp47Folders.get(folderName));
                 String key = locale.getLanguage();
+                assert key != null; // we checked before adding to mBcp47Folders
                 if (locale.hasRegion()) {
                     key = key + '_' + locale.getRegion();
                 }

lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/PluralsDetector.java
@@ -141,6 +141,7 @@ public class PluralsDetector extends ResourceXmlDetector {
             return;
         }
         String language = locale.getLanguage();
+        assert language != null; // checked hasLanguage().
 
         PluralsDatabase plurals = PluralsDatabase.get();
 

sdk-common/src/main/java/com/android/ide/common/resources/AbstractResourceRepository.java
@@ -650,8 +650,9 @@ public abstract class AbstractResourceRepository {
             FolderConfiguration configuration = FolderConfiguration.getConfigForQualifierString(s);
             if (configuration != null) {
                 LocaleQualifier locale = configuration.getLocaleQualifier();
-                if (locale != null && locale.getRegion() != null
-                        && locale.getLanguage().equals(currentLanguage)) {
+                if (locale != null
+                        && locale.getRegion() != null
+                        && currentLanguage.equals(locale.getLanguage())) {
                     set.add(locale.getRegion());
                 }
             }

sdk-common/src/main/java/com/android/ide/common/resources/deprecated/ResourceRepository.java
@@ -759,7 +759,7 @@ public abstract class ResourceRepository {
             for (ResourceFolder folder : folderSubList) {
                 FolderConfiguration config = folder.getConfiguration();
                 LocaleQualifier locale = config.getLocaleQualifier();
-                if (locale != null) {
+                if (locale != null && locale.hasLanguage()) {
                     set.add(locale.getLanguage());
                 }
             }

commit c627dbeed7ff182abe202a5c223706601dc53c83

Fix LocaleQualifier matching that was broken in f8f97fe
This makes it so that two LocaleQualifier objects do not match if
exactly one of them has null language (which is the case for the default
LocaleQualifier)

Change-Id: Ic27d8ee370c8e567f04715242825443670065fba
Fixes: 74431180
Test: Test added in LocaleQualifierTest

sdk-common/src/main/java/com/android/ide/common/resources/configuration/LocaleQualifier.java
@@ -23,6 +23,7 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.Objects;
 
 /**
  * A locale qualifier, which can be constructed from:
@@ -584,9 +585,7 @@ public final class LocaleQualifier extends ResourceQualifier {
         if (qualifier instanceof LocaleQualifier) {
             LocaleQualifier other = (LocaleQualifier)qualifier;
 
-            if (mLanguage != null
-                    && other.mLanguage != null
-                    && !mLanguage.equals(other.mLanguage)) {
+            if (!Objects.equals(mLanguage, other.mLanguage)) {
                 return false;
             }
 

sdk-common/src/test/java/com/android/ide/common/resources/configuration/LocaleQualifierTest.java
@@ -22,9 +22,8 @@ import static com.android.ide.common.resources.configuration.LocaleQualifier.isN
 import static com.android.ide.common.resources.configuration.LocaleQualifier.normalizeCase;
 import static com.android.ide.common.resources.configuration.LocaleQualifier.parseBcp47;
 
-import junit.framework.TestCase;
-
 import java.util.Locale;
+import junit.framework.TestCase;
 
 public class LocaleQualifierTest extends TestCase {
 
@@ -339,5 +338,11 @@ public class LocaleQualifierTest extends TestCase {
         assertFalse(LocaleQualifier.getQualifier("b+en").hasRegion());
         assertFalse(new LocaleQualifier(FAKE_VALUE).hasRegion());
         assertFalse(new LocaleQualifier("", FAKE_VALUE, FAKE_VALUE, null).hasRegion());
+    }
+
+    public void testDefaultMatch() {
+        assertFalse(lq.isMatchFor(getQualifier("ko")));
+        assertFalse(getQualifier("fr").isMatchFor(lq));
+        assertTrue(lq.isMatchFor(new LocaleQualifier()));
     }
 }