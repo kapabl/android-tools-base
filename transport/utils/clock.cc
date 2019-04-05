kapabl/android-tools-base

commit 50dc7ebed3f00b0c669df18948a18a14c65932ec

Minor refactor: removing int64->double->int64 conversion.
Test: manual
Bug: none

Change-Id: I39df76c4dcd3592003256c7bb1ea897174894161

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1e9 * time.tv_sec + time.tv_nsec;
+  return 1000000000 * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler

commit 04c209cb16222a60efc610b39423551a95a9e566

Fix SteadyClock int overflow on 32-bit platforms
Broken by 50dc7ebed3f00b0c669df18948a18a14c65932ec

Bug: N/A
Test: N/A
Change-Id: I6d6697fbafb333d689b8795be020fbba0952b0f2

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1000000000 * time.tv_sec + time.tv_nsec;
+  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler